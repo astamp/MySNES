c9484638a3cbed506bdc5f04a57f2fb04f01f741
Andy Stamp <luigi88s@gmail.com>
2017-03-07 22:31:39 -0500

    [debugger] Delinting

--- mysnes/debugger.py
+++ mysnes/debugger.py
@@ -6,7 +6,7 @@ debugger - Makes heads or tails of all of this.
 
 # Logging setup
 import logging
-log = logging.getLogger(__name__)
+log = logging.getLogger(__name__) # pylint: disable=invalid-name
 log.addHandler(logging.NullHandler())
 
 # Constants
@@ -15,6 +15,7 @@ log.addHandler(logging.NullHandler())
 
 # Classes
 class Debugger(object):
+    """ Debug logging class, intentionally outside of cpu module for speed later. """
     def __init__(self, console):
         self.console = console
         self.cpu = console.cpu
@@ -23,6 +24,7 @@ class Debugger(object):
         self.should_break = False
         
     def poll(self):
+        """ Run one iteration of the debugger, going interactive if single-stepping. """
         self.dump_regs()
         self.preview_next_instruction()
         log.debug("")
@@ -31,15 +33,18 @@ class Debugger(object):
             raw_input()
             
     def dump_regs(self):
+        """ Dump all registers. """
         log.debug("B,A = 0x%02x,0x%02x C = 0x%04x", self.cpu.regs.B, self.cpu.regs.A, self.cpu.regs.C)
         log.debug("X = 0x%04x Y = 0x%04x", self.cpu.regs.X, self.cpu.regs.Y)
         log.debug("DBR = 0x%02x SP = 0x%04x DP = 0x%04x", self.cpu.regs.DBR, self.cpu.regs.SP, self.cpu.regs.DP)
-        log.debug("P = 0x%02x [%s|%s|%s|%s|%s|%s|%s|%s] %s",
+        log.debug(
+            "P = 0x%02x [%s|%s|%s|%s|%s|%s|%s|%s] %s",
             self.cpu.psr.value,
             "N" if self.cpu.psr.negative else "n",
             "O" if self.cpu.psr.overflow else "o",
             "-" if self.cpu.psr.emulation else ("M8" if self.cpu.psr.memory_select else "m16"),
-            ("B" if self.cpu.psr.break_flag else "b") if self.cpu.psr.emulation else ("I8" if self.cpu.psr.memory_select else "i16"),
+            (("B" if self.cpu.psr.break_flag else "b") if self.cpu.psr.emulation else
+             ("I8" if self.cpu.psr.memory_select else "i16")),
             "D" if self.cpu.psr.decimal else "d",
             "I" if self.cpu.psr.irq_disable else "i",
             "Z" if self.cpu.psr.zero else "z",
@@ -49,6 +54,7 @@ class Debugger(object):
         log.debug("PBR:PC = %02x:%04x", self.cpu.regs.PBR, self.cpu.regs.PC)
         
     def preview_next_instruction(self):
+        """ Read next opcode and decode to the mnemonic. """
         opcode = self.mem.read_byte(self.cpu.regs.PBR, self.cpu.regs.PC)
         
         opcode_handler = self.cpu.decode_table.get(opcode, None)
