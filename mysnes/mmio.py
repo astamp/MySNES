40741bec3fd5d7c7cd5fcf1aed1d8b02a3fa18b6
Andy Stamp <luigi88s@gmail.com>
2017-03-12 23:58:04 -0400

    [mmio] Map the APU into the proper registers

--- mysnes/mmio.py
+++ mysnes/mmio.py
@@ -1,8 +1,13 @@
 """
 mmio - Memory mapped I/O registers
+
+https://wiki.superfamicom.org/snes/show/Registers
 """
 
 # Standard library imports
+
+# Local
+from .apu import APU
 
 # Logging setup
 import logging
@@ -10,17 +15,33 @@ log = logging.getLogger(__name__) # pylint: disable=invalid-name
 log.addHandler(logging.NullHandler())
 
 # Constants
+APU_STATUS = 0x2140 # Byte
+APU_DATA = 0x2141 # Byte
+APU_ADDRESS = 0x2142 # Word
 
 # Functions
 
 # Classes
 class MMIO(object):
     """ Mock memory mapped I/O bank for MySNES. """
-    def __getitem__(self, key):
-        # log.debug("MMIO read 0x%04x", key)
-        return 0
+    def __init__(self):
+        self.apu = APU()
+        self.map = {
+            APU_STATUS : (self.apu.read_status, self.apu.write_status),
+            APU_DATA : (self.apu.read_data, self.apu.write_data),
+        }
+    def __getitem__(self, address):
+        handlers = self.map.get(address, None)
+        if handlers:
+            return handlers[0]()
+        else:
+            log.warning("Unhandled MMIO read 0x%04x, returning 0", address)
+            return 0
         
-    def __setitem__(self, key, value):
-        # log.debug("MMIO write 0x%04x -> 0x%02x", key, value)
-        pass
+    def __setitem__(self, address, value):
+        handlers = self.map.get(address, None)
+        if handlers:
+            handlers[1](value)
+        else:
+            log.warning("Unhandled MMIO write 0x%04x -> 0x%02x", address, value)
         
